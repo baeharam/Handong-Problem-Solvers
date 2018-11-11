118f6a4cb8b395b5d34b2df83e855789e72aef85
Byoung-Un-Jeon <44822633+Byoung-Un-Jeon@users.noreply.github.com>
2018-11-11 23:46:31 +0900

    Update Barti.cpp.cpp

--- problems/Dynamic Programming/BOJ9095/Barti.cpp.cpp
+++ problems/Dynamic Programming/BOJ9095/Barti.cpp.cpp
@@ -33,11 +33,13 @@ int count_sum(int temp, int sum)
   //so 5 has 1+2+4+7 = 14? no answer is 13. one's count is omitted.
   //so n's count = (n-1)+(n-2)+(n-3)'s count
   //we can solve problem with recurscive function.
-  if(temp > sum)//if sum is
+  if(temp > sum)//if sum is less than temp that value is useless
     return 0;
 
-  if(temp == sum)
+  if(temp == sum)//if tmpe = sum we find answer
     return 1;
-
+    //The function does not decline the number it receives.
+    //Start at 0 and rise until the entered value is input.
+    //time complexity is n-1
   return count_sum(temp+1, sum) + count_sum(temp+2, sum) + count_sum(temp+3, sum);
 }
