811fcfd2078190ff555cbade8b343865543f7b4a
Odsn <43085342+Odsn@users.noreply.github.com>
2018-12-04 00:27:06 +0900

    BOJ1992_YongJoo

---
+++ problems/BOJ 1922/김용주.cpp
@@ -0,0 +1,25 @@
+//Baekjoon 1202 Odsn
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#if 0
+int N, K;
+vector<int> Mi, Vi, Ci;
+
+
+
+int main(void) {
+
+	cin >> N >> K;
+	for (int i = 0; i < N; i++) 
+		cin >> Mi.push_back >> Vi.push_back;
+	for (int i = 0; i < K; i++)
+		cin >> Ci.push_back;
+
+
+
+	return 0;
+}
+#endif
