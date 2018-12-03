aec2980bde8f43aef2515e07bc0c7e5587db035c
Odsn <43085342+Odsn@users.noreply.github.com>
2018-12-04 01:46:16 +0900

    Update 김용주.cpp

--- problems/BOJ 1922/김용주.cpp
+++ problems/BOJ 1922/김용주.cpp
@@ -1,25 +1,65 @@
-//Baekjoon 1202 Odsn
-#include <iostream>
+//Baekjoon 1992 Odsn
+#include <cstdio>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-#if 0
-int N, K;
-vector<int> Mi, Vi, Ci;
+typedef struct {
+	int a, b, weight;
+}Tree;
+
+Tree t[100001];
+int parent[1001];
+
+int N, M, result;
 
 
+int find(int n) {
+	// ��Ʈ�� �ڱ��ڽ��� return
+	if (parent[n] == n) return n;
+	// ��Ʈ�� �ƴϸ� ��Ʈ�� ã��
+	parent[n] = find(parent[n]);
+	// ��Ʈ�� ���� return
+	return parent[n];
+}
 
-int main(void) {
+bool merge(int a, int b) {
+	a = find(a);
+	b = find(b);
+	
+	// ������ �Ǿ������� false �ƴϸ� ���� ����(����). b�� ��Ʈ�� ��.
+	if (a == b) return false;
+	parent[b] = a;	
+	return true;
+}
 
-	cin >> N >> K;
-	for (int i = 0; i < N; i++) 
-		cin >> Mi.push_back >> Vi.push_back;
-	for (int i = 0; i < K; i++)
-		cin >> Ci.push_back;
+// weight�� �������� �������� sorting�ϱ� ���� �Լ�
+bool compare(const Tree &t1,const Tree &t2) {
+	if (t1.weight < t2.weight)
+		return true;
+	else
+		return false;
+}
 
+int main() {
+	scanf("%d", &N);
+	scanf("%d", &M);
 
+	for (int i = 1; i <= M; i++) 
+		scanf("%d %d %d", &t[i].a, &t[i].b, &t[i].weight);
+
+	// ������ ��� �ڱ��ڽ��� ��Ʈ.
+	for (int i = 1; i <= N; i++)
+		parent[i] = i;
+
+	// weight�� �������� struct�� sort.
+	stable_sort(t + 1, t + M + 1, compare);
+
+	for (int i = 1; i <= M; i++) 
+		if (merge(t[i].a, t[i].b))
+			result += t[i].weight;
+	
+	printf("%d", result);
 
 	return 0;
 }
-#endif
