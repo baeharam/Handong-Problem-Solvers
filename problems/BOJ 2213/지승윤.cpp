8ec4837ac327729175817374e3fb9759ade4fb84
kamining <csy1874@naver.com>
2019-01-29 22:02:37 +0900

    Update 지승윤.cpp

--- problems/BOJ 2213/지승윤.cpp
+++ problems/BOJ 2213/지승윤.cpp
@@ -1,26 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int arr[100]={};
-vector<pair<int,int>> child[100];
-void add(int here, int part){
-	if(!child[here].size()){
-		arr[here] += part;
-		return;
+vector<vector<int>> adj;
+vector<int> MIS[10001][2];
+int N, a, b;
+int maxx[10001][2], w[10001];
+void dfs(int cur, int par) {
+	MIS[cur][1].push_back(cur);
+	maxx[cur][1] = w[cur];
+	for (auto next : adj[cur]) {
+		if (next == par) continue;
+		dfs(next, cur);
+		maxx[cur][1] += maxx[next][0];
+		for (auto i : MIS[next][0]) {
+			MIS[cur][1].push_back(i);
+		}
+		maxx[cur][0] += max(maxx[next][0], maxx[next][1]);
+		int tmp = (maxx[next][0] < maxx[next][1] ? 1 : 0);
+		for (auto i : MIS[next][tmp]) {
+			MIS[cur][0].push_back(i);
+		}
 	}
-	for(auto &p : child[here])
-		add(p.first, part*p.second);
 }
-int main(){
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int n,m;
-	cin >> n >> m;
-	while(m--){
-		int a,b,c;
-		cin >> a >> b >> c;
-		child[a-1].push_back({b-1,c});
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin >> N;
+	for (int i = 1; i <= N; ++i) {
+		cin >> w[i];
 	}
-	add(n-1, 1);
-	for(int i=0; i<n; ++i)
-		if(arr[i]) cout << i+1 << ' ' << arr[i] << '\n';
+	adj.resize(N + 1);
+	for (int i = 0; i < N - 1; ++i) {
+		cin >> a >> b;
+		adj[a].push_back(b);
+		adj[b].push_back(a);
+	}
+	dfs(1, 0);
+	cout << max(maxx[1][0], maxx[1][1]) << '\n';
+	int tmp = (maxx[1][0] < maxx[1][1] ? 1 : 0);
+	
+	sort(MIS[1][tmp].begin(), MIS[1][tmp].end());
+	for (auto i : MIS[1][tmp]) {
+		cout << i << ' ';
+	}	
 }
