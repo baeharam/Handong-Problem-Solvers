b466b8c75fe99254337577330a75f550908ab8bc
Hayun218 <44897331+Hayun218@users.noreply.github.com>
2019-02-12 01:16:52 +0900

    Update 박하윤.cpp

--- problems/BOJ 2003/박하윤.cpp
+++ problems/BOJ 2003/박하윤.cpp
@@ -1,4 +1,4 @@
-#include <cstdio>
+/*#include <cstdio>
 using namespace std;
 
 int main(){
@@ -7,7 +7,7 @@ int main(){
     for(int i = 0; i < n; i++)
         scanf("%d", a+i);
         
-    s = 0; e = 0; result = 0;
+    s = 0; e = 0; result = 0; sum = 0;
     while(1){
         if(sum >= m) sum -= a[s++];
         else if(e == n) break;
@@ -16,18 +16,21 @@ int main(){
     }
     printf("%d\n", result);
     return 0;
-}
-/*#include <cstdio>
+}*/ //. 두가지 방법으로 구현해봄 => 둘다 수정 후 정상적으로 running
+
+#include <cstdio>
+#include <algorithm>
+using namespace std;
 
 int main(){
-    int n, m, a[10001], sum, result;
-    int s;
+    int n, m, a[10001], sum, result, s;
     scanf("%d %d", &n, &m);
 
+    sum = 0; result = 0, s = 0;
     for(int i = 0; i < n; i++){
         scanf("%d", a+i);
         sum += a[i];
-        while(sum > m)
+        while(sum > m && s < n)
             sum -= a[s++];
         if(sum == m)
             result++;
@@ -35,4 +38,4 @@ int main(){
     
     printf("%d\n", result);
     return 0;
-}*/
+}
